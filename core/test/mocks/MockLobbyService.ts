b4e41f125de862a7c61709e5be484029198eb92d
Alex Slater <dev@alexslater.io>
2024-08-10 11:00:15 +0100

    fix some build errors

--- core/test/mocks/MockLobbyService.ts
+++ core/test/mocks/MockLobbyService.ts
@@ -1,23 +1,29 @@
 import { generateResourceId, MockOf } from "@alexslater-io/common";
 import { LobbyService } from "../../src/lib/services/LobbyService";
-import { Lobby } from '../../src/lib/types/Lobby';
-\export class MockLobbyService implements MockOf<LobbyService> {
-	public reset(): void {
-		console.log("Resetting Mock Lobby Service");
-		this.mockedCreateResult = {
-			id: generateResourceId("gaming", "lobby"),
-		};
-		this.mockedListResult = [];
-		this.numOfCreateCalls = 0;
+import { Lobby } from "../../src/lib/types/Lobby";
+export class MockLobbyService implements MockOf<LobbyService> {
+	constructor() {
+		this.reset();
 	}
+
 	public get mock() {
 		return this as unknown as LobbyService;
 	}
 
-	public mockedCreateResult: Lobby = {
-		id: generateResourceId("gaming", "lobby"),
-	};
-	public numOfCreateCalls: number = 0;
+	public mockedCreateResult: Lobby;
+	public numOfCreateCalls: number;
+	public mockedListResult: Lobby[];
+	public reset(): void {
+		console.log("Resetting Mock Lobby Service");
+		this.mockedCreateResult = {
+			id: generateResourceId("gaming", "lobby"),
+			minPlayers: 0,
+			maxPlayers: 5,
+		};
+		this.mockedListResult = [];
+		this.numOfCreateCalls = 0;
+	}
+
 	public create(): Lobby {
 		this.numOfCreateCalls++;
 		return this.mockedCreateResult;
@@ -26,8 +32,6 @@ import { Lobby } from '../../src/lib/types/Lobby';
 		console.log("Storing lobby: ", lobby);
 		return;
 	}
-
-	public mockedListResult: Lobby[] = [];
 	public async list(): Promise<Lobby[]> {
 		console.log("Listing lobbies");
 		return this.mockedListResult;
